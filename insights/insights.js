5ae810e32b7279131ac5356d7824816dea90a9f3
marknorth8-code <marknorth8@gmail.com>
2026-01-26 17:16:23 -0300

    Delete insights/insights.js

--- insights/insights.js
+++
@@ -1,8 +0,0 @@
-
-// Basic placeholder for carousel logic
-// Expand to multiple cards as content grows
-fetch("../header.html")
-  .then(response => response.text())
-  .then(data => {
-    document.getElementById("header").innerHTML = data;
-  });
