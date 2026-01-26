a8f4e662406f37cad1fcc315a64a38c347f084f6
marknorth8-code <marknorth8@gmail.com>
2026-01-26 08:23:15 -0300

    Update insights.js

--- insights/insights.js
+++ insights/insights.js
@@ -1,3 +1,8 @@
 
 // Basic placeholder for carousel logic
 // Expand to multiple cards as content grows
+fetch("../header.html")
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById("header").innerHTML = data;
+  });
